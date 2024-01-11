3e52430c3314fc48fe81cd00b97769ca15ff9b27
liuq19 <liuqiang.06@bytedance.com>
2024-01-11 11:40:34 +0800

    feat: support prelude for json traits

---
+++ src/prelude.rs
@@ -0,0 +1,7 @@
+//! Imports the various traits about JSON. `use sonic_rs::prelude::*`` to make the
+//! various traits and methods imported if you need.
+
+pub use crate::{
+    serde::JsonNumberTrait,
+    value::{JsonContainerTrait, JsonValueMutTrait, JsonValueTrait},
+};
