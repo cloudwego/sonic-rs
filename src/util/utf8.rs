565b65036644efd4cddd6e7997b9ec377e6f9593
liuq19 <liuqiang.06@bytedance.com>
2024-11-01 15:17:21 +0800

    test: make miri happy in lazyvalue

--- src/util/utf8.rs
+++ src/util/utf8.rs
@@ -1,5 +1,7 @@
 use crate::error::{Error, ErrorCode, Result};
 
+// simduft8 will cause `out-of-bounds pointer arithmetic` when using Miri tests
+#[cfg(not(miri))]
 #[inline(always)]
 pub(crate) fn from_utf8(data: &[u8]) -> Result<&str> {
     match simdutf8::basic::from_utf8(data) {
@@ -11,6 +13,19 @@ pub(crate) fn from_utf8(data: &[u8]) -> Result<&str> {
     }
 }
 
+#[cfg(miri)]
+pub(crate) fn from_utf8(data: &[u8]) -> Result<&str> {
+    match std::str::from_utf8(data) {
+        Ok(ret) => Ok(ret),
+        Err(err) => Err(Error::syntax(
+            ErrorCode::InvalidUTF8,
+            data,
+            err.valid_up_to(),
+        )),
+    }
+}
+
+#[cfg(not(miri))]
 #[cold]
 fn from_utf8_compat(data: &[u8]) -> Result<&str> {
     // compat::from_utf8 is slower than basic::from_utf8
