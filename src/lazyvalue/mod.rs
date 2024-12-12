aa03ecbb0202c071edb2be14a8e2bd19714adab7
liuqiang.06 <liuqiang.06@bytedance.com>
2024-12-12 14:42:44 +0800

    fmt

--- src/lazyvalue/mod.rs
+++ src/lazyvalue/mod.rs
@@ -8,7 +8,7 @@ pub(crate) mod value;
 #[doc(inline)]
 pub use self::{
     get::{
-        get, get_from_bytes, get_from_bytes_unchecked, get_from_faststr,
+        advance, get, get_from_bytes, get_from_bytes_unchecked, get_from_faststr,
         get_from_faststr_unchecked, get_from_slice, get_from_slice_unchecked, get_from_str,
         get_from_str_unchecked, get_many, get_many_unchecked, get_unchecked,
     },
@@ -16,9 +16,9 @@ pub use self::{
         to_array_iter, to_array_iter_unchecked, to_object_iter, to_object_iter_unchecked,
         ArrayJsonIter, ObjectJsonIter, OwnedArrayJsonIter, OwnedObjectJsonIter,
     },
-    owned::OwnedLazyValue,
     owned::LazyArray,
     owned::LazyObject,
+    owned::OwnedLazyValue,
     value::LazyValue,
 };
 
