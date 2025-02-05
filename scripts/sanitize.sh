fc44cb760eb0e28e655d7e467f68c2769a88a928
Pure White <wudi.daniel@bytedance.com>
2025-02-05 20:59:24 +0800

    try use matrix to optimize sanitize speed

--- scripts/sanitize.sh
+++ scripts/sanitize.sh
@@ -11,5 +11,5 @@ run_tests() {
     cargo +nightly test --doc --package sonic-rs --target x86_64-unknown-linux-gnu --features "$features" -- --show-output --test-threads=1
 }
 
-echo "Running tests with $san and $feature"
-RUSTFLAGS="-Zsanitizer=$1" RUSTDOCFLAGS="-Zsanitizer=$2" run_tests $1 $2
+echo "Running tests with $1 and $2"
+RUSTFLAGS="-Zsanitizer=$1" RUSTDOCFLAGS="-Zsanitizer=$1" run_tests $1 $2
