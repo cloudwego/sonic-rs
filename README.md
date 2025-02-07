1e76404a04436d8d55eb890f6bed212dd7cc156f
liuqiang <liuqiang.06@bytedance.com>
2025-02-07 16:07:39 +0800

    feat: add sanitize feature

--- README.md
+++ README.md
@@ -25,6 +25,8 @@ A fast Rust JSON library based on SIMD. It has some references to other open-sou
 
 3. Please add the compile options `-C target-cpu=native`
 
+4. Should enable `sanitize` feature if you are using LLVM-sanitizer in your program.
+
 ## Quick to use sonic-rs
 
 To ensure that SIMD instruction is used in sonic-rs, you need to add rustflags `-C target-cpu=native` and compile on the host machine. For example, Rust flags can be configured in Cargo [config](.cargo/config.toml).
--- scripts/sanitize.sh
+++ scripts/sanitize.sh
@@ -5,11 +5,9 @@ set -ex
 export ASAN_OPTIONS="disable_coredump=0:unmap_shadow_on_exit=1:abort_on_error=1"
 
 run_tests() {
-    local san="$1"
-    local features="$2"
-    cargo +nightly test --target x86_64-unknown-linux-gnu --features "$features"  -- --test-threads=1 --nocapture
-    cargo +nightly test --doc --package sonic-rs --target x86_64-unknown-linux-gnu --features "$features" -- --show-output --test-threads=1
+    cargo +nightly test --release --target x86_64-unknown-linux-gnu --features sanitize "$2"
+    cargo +nightly test --doc --package sonic-rs --target x86_64-unknown-linux-gnu --features sanitize "$2"
 }
 
 echo "Running tests with $1 and $2"
-RUSTFLAGS="-Zsanitizer=$1" RUSTDOCFLAGS="-Zsanitizer=$1" run_tests $1 $2
+RUSTFLAGS="-Zsanitizer=$1" RUSTDOCFLAGS="-Zsanitizer=$1" run_tests "$2"
