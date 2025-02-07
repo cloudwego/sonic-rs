94137f4a3833d2350b6730caeb4651519812eabe
Pure White <wudi.daniel@bytedance.com>
2025-02-07 16:47:34 +0800

    ci: fix sanitize

--- scripts/sanitize.sh
+++ scripts/sanitize.sh
@@ -5,8 +5,8 @@ set -ex
 export ASAN_OPTIONS="disable_coredump=0:unmap_shadow_on_exit=1:abort_on_error=1"
 
 run_tests() {
-    cargo +nightly test --release --target x86_64-unknown-linux-gnu --features sanitize "$2"
-    cargo +nightly test --doc --package sonic-rs --target x86_64-unknown-linux-gnu --features sanitize "$2"
+    cargo +nightly test --release --target x86_64-unknown-linux-gnu --features sanitize,"$1"
+    cargo +nightly test --doc --package sonic-rs --target x86_64-unknown-linux-gnu --features sanitize,"$1"
 }
 
 echo "Running tests with $1 and $2"
